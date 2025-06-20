4ddb1c1d609d38db022205fea236afb53e6af6cd
Bo Hulshof <51164986+BoMeneerNL@users.noreply.github.com>
2025-06-20 11:56:57 +0200

    fixed typo

--- example.env.php
+++ example.env.php
@@ -1,7 +1,7 @@
 <?php
 /* 
 *    to use this as env file, copy this file in the src directory and rename it to env.php
-*    #if used in Docker context the host is the name of the service in the docker-compose file,
+*    if used in Docker context the host is the name of the service in the docker-compose file,
 *    not localhost
 */
 $db_host = "";
