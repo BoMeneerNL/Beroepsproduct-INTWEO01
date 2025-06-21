4c9d4e8916c1ea987a6b8747190cf730dc447326
Bo Hulshof <51164986+BoMeneerNL@users.noreply.github.com>
2025-06-21 21:49:15 +0200

    Added a template engine

--- src/index.php
+++ src/index.php
@@ -1,2 +1,9 @@
 <?php
 require_once 'sqlhandler.php';
+require_once 'templates/templateloader.php';
+printTemplate('pre_content', ["siteTitle" => "Main page"]);
+?>
+
+
+<?php
+printTemplate('post_content');
