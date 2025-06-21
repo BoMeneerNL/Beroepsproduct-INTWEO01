69b5075832e36a623acc1aa8b05e67287e97da28
Bo Hulshof <51164986+BoMeneerNL@users.noreply.github.com>
2025-06-21 22:33:36 +0200

    temp removed phpdoc

--- src/sqlhandler.php
+++ src/sqlhandler.php
@@ -4,14 +4,6 @@ require_once 'utils.php';
 require_once 'env.php';
 
 disallowDirectAccess(__FILE__);
-
-/**
- * Executes a SQL query and returns the result as an array.
- *
- * @param string $query The SQL query to execute.
- * @param array $params The parameters to bind to the query.
- * @return array The result set as an associative array.
- */
 function getWithSql(string $query = "", array|null $params = null): array
 {
     $pointer = execute($query, $params);
@@ -21,27 +13,10 @@ function getWithSql(string $query = "", array|null $params = null): array
     return $pointer->fetchAll();
 }
 
-/**
- * Executes a SQL query without returning any result.
- *
- * @param string $query The SQL query to execute.
- * @param array $params The parameters to bind to the query.
- * @return void
- */
 function executeWithSql(string $query = "", array|null $params = null): void
 {
     execute($query, $params);
 }
-
-/**
- * !USE ONE OF THE HELPER FUNCTIONS (executeWithSql() or getWithSql()), NOT THIS FUNCTION DIRECTLY!
- * 
- * Executes a SQL query and returns the PDOStatement object. 
- *
- * @param string $query The SQL query to execute.
- * @param array $params The parameters to bind to the query.
- * @return bool|PDOStatement Returns false on failure, or the PDOStatement object on success.
- */
 
 function execute(string $query = "", array|null $params = null): bool|PDOStatement
 {
