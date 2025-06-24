37c9ef0d98699e0a587e89a50d8bf55424e8599b
Bo Hulshof <51164986+BoMeneerNL@users.noreply.github.com>
2025-06-24 14:08:39 +0200

    Made SQLHandler have the connection as class (for less connections)

--- src/sqlhandler.php
+++ src/sqlhandler.php
@@ -1,35 +1,46 @@
 <?php
 
 require_once 'utils.php';
+disallowDirectAccess(__FILE__);
+
 require_once 'env.php';
 
-disallowDirectAccess(__FILE__);
-function getWithSql(string $query = "", array|null $params = null): array
-{
-    $pointer = execute($query, $params);
-    if ($pointer === false) {
-        return [];
-    }
-    return $pointer->fetchAll();
-}
 
-function executeWithSql(string $query = "", array|null $params = null): void
+class SQLConnection
 {
-    execute($query, $params);
-}
-
-function execute(string $query = "", array|null $params = null): bool|PDOStatement
-{
-    if (empty($query)) {
-        return false;
+    private $conn;
+    public function __construct()
+    {
+        global $db_host, $db_port, $db_user, $db_pass, $db_database;
+        $this->conn = new PDO(
+            "mysql:host=$db_host;port=$db_port;dbname=$db_database;charset=utf8",
+            $db_user,
+            $db_pass
+        );
     }
 
-    global $db_host, $db_port, $db_user, $db_pass, $db_database;
+    function getWithSql(string $query = "", array|null $params = null): array
+    {
+        $pointer = $this->execute($query, $params);
+        if ($pointer === false) {
+            return [];
+        }
+        return $pointer->fetchAll();
+    }
 
-    $con = new PDO(
-        "mysql:host=$db_host;port=$db_port;dbname=$db_database;charset=utf8",$db_user,$db_pass
-    );
-    $stmt = $con->prepare($query);
-    $stmt->execute($params);
-    return $stmt;
+    function executeWithSql(string $query = "", array|null $params = null): void
+    {
+        $this->execute($query, $params);
+    }
+
+    function execute(string $query = "", array|null $params = null): bool|PDOStatement
+    {
+        if (empty($query)) {
+            return false;
+        }
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($params);
+        return $stmt;
+    }
+
 }
