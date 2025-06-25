fc0435fcd331a1376bdfde63f899a541da37a77f
Bo Hulshof <51164986+BoMeneerNL@users.noreply.github.com>
2025-06-25 03:28:44 +0200

    Added createAccount API Route

---
+++ src/api/createAccount.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $error = urlencode('Wrong Request Method On Request');
+    header("Location: /register?error={$error}", true, 307);
+    exit;
+}
+if (!isset($_POST['username']) || !isset($_POST['password'])) {
+    header("Location: /register?error=" . urlencode('Field Validation Failed'), true, 307);
+    exit;
+}
+
+
+
+
+require_once __DIR__ . '/../sqlhandler.php';
+
+$db = new SQLConnection();
+
+$countForUsername = $db->getWithSql("SELECT COUNT(*) AS count FROM User WHERE username = :username", ['username' => $_POST['username']]);
+$usernameExists = $countForUsername[0]['count'] > 0;
+if ($usernameExists) {
+    header("Location: /register?error=" . urlencode('Username Already Exists'), true, 307);
+}
+$db->destroy();
+
--- src/register.php
+++ src/register.php
@@ -3,7 +3,7 @@ require_once "loaders/templateloader.php";
 printTemplate("pre_content", templateData: ["siteTitle" => "Register", "lang" => "nl"]);
 printTemplate("load_css", templateData: ["css_location" => "/styles/forms.css"]);
 ?>
-<form method="post" action="register.php">
+<form method="post" action="/api/createAccount">
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" required>
 
