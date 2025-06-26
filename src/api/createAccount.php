0beda56a4e3c810fa6aaab7947130d455d7c1ed7
Bo Hulshof <51164986+BoMeneerNL@users.noreply.github.com>
2025-06-26 02:57:09 +0200

    Updated register + Added name

--- src/api/createAccount.php
+++ src/api/createAccount.php
@@ -22,10 +22,27 @@ require_once __DIR__ . '/../sqlhandler.php';
 
 $db = new SQLConnection();
 
-$countForUsername = $db->getWithSql("SELECT COUNT(*) AS count FROM User WHERE username = :username", ['username' => $_POST['username']]);
+$countForUsername = $db->getWithSql(
+    "SELECT COUNT(*) AS count FROM User WHERE username = :username",
+    ['username' => $_POST['username']]
+);
+
 $usernameExists = $countForUsername[0]['count'] > 0;
 if ($usernameExists) {
-    header("Location: /register?error=" . urlencode('Username Already Exists'), true, 307);
+    header("Location: /register?error=" . urlencode('Username Already Exists'), true, response_code: 307);
+    exit;
 }
+$creationParams = [
+    'username' => $_POST['username'],
+    'password' => password_hash($_POST['password'], PASSWORD_ARGON2ID),
+    'first_name' => $_POST['first_name'] ?? '',
+    'last_name' => $_POST['last_name'] ?? ''
+];
+$db->executeWithSql(
+    "INSERT INTO User (username, password,first_name,last_name, role) VALUES (:username, :password,:first_name,:last_name,'Client')",
+    $creationParams
+);
+
 $db->destroy();
+header("Location: /login?success=" . urlencode('Account Created Successfully, you can now login here'), true, response_code: 307);
 
--- src/register.php
+++ src/register.php
@@ -8,16 +8,27 @@ printTemplate("pre_content", templateData: ["siteTitle" => "Register", "lang" =>
 printTemplate("load_css", templateData: ["css_location" => "/styles/forms.css"]);
 
 ?>
-
-<form method="post" action="/api/createAccount">
+<div class="center-content-middle">
+<form method="post" action="/api/createAccount" class="form-container">
     <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
+    <input type="text" id="username" name="username" required autocomplete="username">
 
     <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
+    <input type="password" id="password" name="password" required autocomplete="new-password">
 
+    <div class="grid-container">
+        <div>
+            <label for="first_name">First Name:</label>
+            <input type="text" id="first_name" name="first_name" required>
+        </div>
+
+        <div>
+            <label for="last_name">Last Name:</label>
+            <input type="text" id="last_name" name="last_name" required>
+        </div>
+    </div>
     <input type="submit" value="Register">
 </form>
-
+</div>
 <?php
 printTemplate("post_content");
